4a353cd07e7cbb3e695d5b0b1604b88f33096590
sridinesh_20 <127740826+dinesh2034@users.noreply.github.com>
2025-02-05 12:22:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u sridinesh -p 123456789
+    docker tag test sridinesh/dinesh_20
+    docker push sridinesh/dinesh_20
     
